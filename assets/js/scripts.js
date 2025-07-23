c324fcc7dd949f5831e7d7e6e07ba81e2115f6b8
Wesley <wesley.analistasystem@gmail.com>
2025-07-22 23:39:07 -0300

    Décimo nono commit do not para o gh

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,3 +13,14 @@ function changeTheme() {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () =>  {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+        
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
+    })
+})
