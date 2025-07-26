d625a4ccef2f169d6d2dd438616af65d10e0fe2a
Wesley <wesley.analistasystem@gmail.com>
2025-07-26 01:50:11 -0300

    Vigésimo Sexto Commit foi modificado o tema dark

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,29 @@
 const toggleTheme = document.getElementById("toggleTheme");
-const rootHtml = document.documentElement
+const rootHtml = document.documentElement;
 
 function changeTheme() {
     const currentTheme = rootHtml.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light": "dark";
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : 
-    rootHtml.setAttribute("data-theme", "dark")
+    rootHtml.setAttribute("data-theme", newTheme)
+    localStorage.setItem("theme", newTheme); 
 
-    toggleTheme.classList.toggle("bi-sun")
-    toggleTheme.classList.toggle("bi-moon-stars")
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon");
 
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+const savedTheme = localStorage.getItem("theme") || "dark";
+rootHtml.setAttribute("data-theme", savedTheme);
+
+toggleTheme.classList.toggle("bi-sun", savedTheme === "dark");
+toggleTheme.classList.toggle("bi-moon", savedTheme === "light");
+})
 
 const accordionHeaders = document.querySelectorAll(".accordion__header");
 
@@ -21,9 +32,9 @@ accordionHeaders.forEach(header => {
         const accordionItem = header.parentElement;
         const accordionActive = accordionItem.classList.contains("active");
         
-        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active")
-    })
-})
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    });
+});
 
 const menuLinks = document.querySelectorAll(".menu__link");
 
@@ -31,5 +42,9 @@ menuLinks.forEach(item => {
     item.addEventListener("click", () => {
         menuLinks.forEach(i => i.classList.remove("active"));
         item.classList.add("active")
-    })
-})
+    });
+});
+
+
+
+
