9ed3ebff021a03dd810a3565f36df30a55cb694d
wesley-guilherme <wesley.analistasystem@gmail.com>
2025-07-28 16:13:52 -0300

    Commit corrigir name e title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,15 @@
 function updatePortInfo(portData) {
-    const title = document.getElementById('port.title')
+    const title = document.getElementById('port-title')
     title.innerText = portData.title
+}
+
+function updatePortName(portData) {
+    const name = document.getElementById('port-name')
+    name.innerText = portData.name
 }
 
 (async() => {
     const portData = await fetchPortData()
     updatePortInfo(portData)
-})
+    updatePortName(portData)
+})();
