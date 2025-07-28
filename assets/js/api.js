bdc80d15b097a9301f2514655b9a46c7e2008254
wesley-guilherme <wesley.analistasystem@gmail.com>
2025-07-28 17:32:17 -0300

    commit correção link json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchPortData() {
-    const url = 'https://raw.githubusercontent.com/wesley-guilherme/wesleyguilherme.github.io/refs/heads/main/assets/js/main.json';
+    const url = 'https://github.com/wesley-guilherme/wesleyguilherme.github.io/blob/main/data/port.json';
     const fetching = await fetch(url);
     return await fetching.json();   
 }
