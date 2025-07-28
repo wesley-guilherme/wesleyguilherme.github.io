2daaaa09363a7b7779bacb4d3c83b6b051b62f6c
wesley-guilherme <wesley.analistasystem@gmail.com>
2025-07-28 17:33:44 -0300

    correção api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchPortData() {
     const url = 'https://github.com/wesley-guilherme/wesleyguilherme.github.io/blob/main/data/port.json';
-    const fetching = await fetch(url);
-    return await fetching.json();   
+    const response = await fetch(url);
+    return await response.json();   
 }
  
